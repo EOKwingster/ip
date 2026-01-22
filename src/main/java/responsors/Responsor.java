951e668ddaf290096f959c0bd4a4fa034e3e2185
EOKwingster <y1709175257@outlook.com>
2026-01-22 15:29:28 +0800

    add willExit() in Responsor interface

--- src/main/java/responsors/Responsor.java
+++ src/main/java/responsors/Responsor.java
@@ -8,9 +8,17 @@ import java.util.List;
  */
 public interface Responsor {
     /**
-     *
+     * The response logic of this Responsor
      * @param input The String message that user inputs
      * @return Response messages as a list of String, each element is one line.
      */
     List<String> response(String input);
+
+    /**
+     * Distinguish between Responsors that make chat close and the others.
+     * @return if exiting chat after response, default to false.
+     */
+    default boolean willExit() {
+        return false;
+    }
 }
