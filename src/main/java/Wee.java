e781ed5c8a4a5a61133c7c5609d151daf8f6faca
EOKwingster <y1709175257@outlook.com>
2026-01-22 14:05:04 +0800

    add say() method to output formatted messages

--- src/main/java/Wee.java
+++ src/main/java/Wee.java
@@ -1,4 +1,9 @@
 public class Wee {
+    private static final Wee WEE = new Wee();
+    public static final String NAME = "Wee";
+
+    private Wee() {}
+
     public static void main(String[] args) {
         String logo = """
                 ██     ██  ████████  ████████\s
@@ -8,5 +13,17 @@ public class Wee {
                  ███ ███   ████████  ████████
                 """;
         System.out.println("Hello from\n" + logo);
+        WEE.say("Hello! I'm " + NAME, "What can I do for you");
+    }
+
+    private void say(String... messages) {
+        String speaker = NAME + " >> ";
+        String indentation = " ".repeat(speaker.length());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < messages.length; i++) {
+            String prefix = i == 0 ? speaker : indentation;
+            stringBuilder.append(prefix).append(messages[i]).append("\n");
+        }
+        System.out.println(stringBuilder);
     }
 }
