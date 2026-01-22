0258593de22b52e2c189a3ef61f78c659a0bf4e1
EOKwingster <y1709175257@outlook.com>
2026-01-22 14:48:43 +0800

    add Responsor interface to define response behavior

---
+++ src/main/java/responsors/Responsor.java
@@ -0,0 +1,16 @@
+package responsors;
+
+import java.util.List;
+
+/**
+ * define the universal response behavior。
+ * Any response logic should be inside a class implements this interface.
+ */
+public interface Responsor {
+    /**
+     *
+     * @param input The String message that user inputs
+     * @return Response messages as a list of String, each element is one line.
+     */
+    List<String> response(String input);
+}
