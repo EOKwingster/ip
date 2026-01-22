d25a61d9b0612883be418cab6c13a5b014ef749e
EOKwingster <y1709175257@outlook.com>
2026-01-22 17:29:51 +0800

    make ChatData supports storing an inputs list

--- src/main/java/com/eokwingster/data/ChatData.java
+++ src/main/java/com/eokwingster/data/ChatData.java
@@ -1,8 +1,19 @@
 package com.eokwingster.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class store all the data needed and generated in a chat.
  */
 public class ChatData {
+    private final List<String> inputsReceived = new ArrayList<>();
 
+    public void storeInput(String input) {
+        inputsReceived.add(input);
+    }
+
+    public List<String> getInputsReceived() {
+        return inputsReceived;
+    }
 }
