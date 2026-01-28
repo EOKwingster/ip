77d44050d2de824ce2a3ecfd876a2e8d7156392b
EOKwingster <y1709175257@outlook.com>
2026-01-28 15:09:30 +0800

    Fix minor bug: wrong computation of task index

--- src/main/java/com/eokwingster/responsor/responsors/TaskDeleteResponsor.java
+++ src/main/java/com/eokwingster/responsor/responsors/TaskDeleteResponsor.java
@@ -2,6 +2,7 @@ package com.eokwingster.responsor.responsors;
 
 import com.eokwingster.command.Step;
 import com.eokwingster.data.ChatData;
+import com.eokwingster.data.task.Task;
 import com.eokwingster.responsor.Response;
 import com.eokwingster.responsor.Responsor;
 
@@ -10,9 +11,10 @@ import java.util.List;
 public class TaskDeleteResponsor implements Responsor {
     @Override
     public Response.Builder response(String argument, ChatData chatData, Response.Builder builtResponse, List<Step> steps) {
+        Task task = chatData.getTasks().remove(Integer.parseInt(argument) - 1);
         builtResponse.appendMessages(
                 "This task has been removed:",
-                "  " + chatData.getTasks().get(Integer.parseInt(argument)),
+                "  " + task,
                 "Now you have " + chatData.getTasks().size() + " tasks."
         );
         return builtResponse;
